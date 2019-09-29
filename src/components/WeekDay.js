762c343add4aed8915c380fe9621df39fe735d99
Ikhyeon <kih199212@gmail.com>
2019-09-29 19:09:50 +0900

    Add time table

--- src/components/WeekDay.js
+++ src/components/WeekDay.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export default function Day(){
+export default function Day(props){
+
+    const renderSlot = (day) => {
+        const slots = []
+        slots.push(<div className="daySlots days">{day}</div>)
+        for(let i = 0; i< 48; i++){
+            const slot = <div key={i} className="daySlot">&nbsp;</div>
+            slots.push(slot)
+        }
+        console.log(slots.length)
+        return slots
+    }
     return (
-        <div></div>
+        <div className="daySlots">{ renderSlot(props.day) }</div>
     )
 }
--- src/components/WeekDays.js
+++ src/components/WeekDays.js
@@ -4,11 +4,50 @@ import Day from './WeekDay'
 export default class Days extends React.Component {
     renderDays = () => {
         // rendering week days monday, tuesday ...
+        const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        const daysSlots = []
+
+        days.forEach( day => {
+            const slot = <Day day={day}/>
+            daysSlots.push(slot)
+        })
+
+        return daysSlots
+    }
+    renderTime = () => {
+        const times = []
+        let hour = 0
+        let minute = 0
+        times.push(<div className="dayTime time">Time</div>)
+        for(let i = 0; i< 48; i++){
+            let hourText;
+            let minuteText;
+            if(hour < 10){
+                hourText = `${hour}`
+            }else if(hour > 12){
+                hourText = `${hour - 12}`
+            }else{
+                hourText =`${hour}`
+            }
+            minute === 0 ? minuteText = `${minute}0` : minuteText = `${minute}`
+
+            const timeSlot = hour < 12 ? <div key={i} className="dayTime"><span>{`${hourText}:${minuteText} AM`}</span></div> :
+                                        <div key={i} className="dayTime"><span>{`${hourText}:${minuteText} PM`}</span></div>
+            times.push(timeSlot)
+            if(minute === 0){
+                minute = 30
+            }else{
+                minute = 0
+                hour++
+            }
+        }
+        return times
     }
     render(){
         return(
-            <div>
-                
+            <div className="timeTable">
+                <div className="dayTimes">{ this.renderTime() }</div>
+                { this.renderDays() }
             </div>
         )
     }
