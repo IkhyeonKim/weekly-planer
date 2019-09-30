01a4e865ec98b6c7b306c248a9244524b88a4c05
Ikhyeon <kih199212@gmail.com>
2019-09-30 18:41:41 +0900

    Print time and note

--- src/components/WeekDay.js
+++ src/components/WeekDay.js
@@ -13,9 +13,7 @@ export default function Day(props){
         if(props.dayPlan.length !== 0){
             timeIndex = 0
             isStillGoing = false
-        }
-        console.log(props.dayPlan[timeIndex])
-        
+        }        
 
         slots.push(<div key={day} className="daySlots days">{day}</div>)
         for(let i = 0; i< 48; i++){
@@ -23,17 +21,30 @@ export default function Day(props){
             let hourText = hour < 10 ? `0${hour}` : `${hour}`
             let minuteText = minute === 0 ? `0${minute}` : `${minute}`
             let slotKey = `${hourText}:${minuteText}`
+            let printedText = undefined
 
             // console.log(`${hourText}:${minuteText}`)
             if(props.dayPlan[timeIndex]){
                 // to paint a slot
+                const reg = /\d{2}/
+                const regMinute = /\d{2}$/
+
+                const startTime = parseInt(reg.exec(props.dayPlan[timeIndex].start)[0])
+                const endTime = parseInt(reg.exec(props.dayPlan[timeIndex].end)[0])
+                const startTimeMinute = parseInt(regMinute.exec(props.dayPlan[timeIndex].end)[0])
+                const endTimeMinute = parseInt(regMinute.exec(props.dayPlan[timeIndex].end)[0])
+
                 if(slotKey === props.dayPlan[timeIndex].start){
                     colored = 'color'
                     isStillGoing = true
+                    let startAmPm = startTime < 12 ? 'AM' : 'PM' 
+                    let endAmPm = endTime < 12 ? 'AM' : 'PM' 
+                    printedText = `${startTime > 12 ? startTime - 12: startTime}:${startTimeMinute} ${startAmPm} - ${endTime > 12 ? endTime - 12: endTime}:${endTimeMinute} ${endAmPm}`
+                    printedText += `\n${props.dayPlan[timeIndex].note}`
                 }
 
             }
-            const slot = <div key={slotKey} className={colored || isStillGoing ? `daySlot color`: `daySlot`}></div>
+            const slot = <div key={slotKey} className={colored || isStillGoing ? `daySlot color`: `daySlot`}>{printedText?printedText:''}</div>
             slots.push(slot)
             
             if(props.dayPlan[timeIndex]){
